0988ac7b1bc573b7efae1cc1eb44e30a72de0a4c
Dmitry Muhomor <muhomor.dmitry@gmail.com>
2023-10-26 09:47:43 +0300

    declare ARM MTE support
    This is a non-AOSP property, it requires patches to build/make and build/soong.

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -17,6 +17,7 @@ TARGET_ARCH := arm64
 TARGET_ARCH_VARIANT := armv8-2a
 TARGET_CPU_ABI := arm64-v8a
 TARGET_CPU_VARIANT := cortex-a55
+TARGET_HAS_ARM_MTE := true
 
 BOARD_BOOTCONFIG += \
     androidboot.boot_devices=13200000.ufs
