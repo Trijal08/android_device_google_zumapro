0a9f3f34f50b72d1d5544b2f04799eae265abe70
Daniel Micay <daniel.micay@grapheneos.org>
2023-12-03 07:20:11 -0500

    use more modern target CPU configuration

--- BoardConfig-common.mk
+++ BoardConfig-common.mk
@@ -14,9 +14,9 @@ include device/google/zumapro/BoardConfig-16k-common.mk
 endif
 
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a
+TARGET_ARCH_VARIANT := armv8-2a-dotprod
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-a55
+TARGET_CPU_VARIANT := cortex-a76
 TARGET_HAS_ARM_MTE := true
 BOARD_KERNEL_CMDLINE += bootloader.pixel.MTE_FORCE_ON
 
