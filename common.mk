292cc36ff86e33a370a420ca78b5b730c8a110ff
Ionut Gherman <ghermanionut96@gmail.com>
2024-08-13 11:19:18 +0300

    zuma: include Google Face Unlock if exists
    Signed-off-by: Ionut Gherman <ghermanionut96@gmail.com>

--- common.mk
+++ common.mk
@@ -378,6 +378,9 @@ PRODUCT_COPY_FILES += \
 PRODUCT_PACKAGES += \
     android.hardware.location.gps.prebuilt.xml
 
+# Google Face Unlock
+$(call inherit-product-if-exists, vendor/google/faceunlock/config.mk)
+
 # Pixel Camera
 $(call inherit-product-if-exists, vendor/google/camera/camera.mk)
 
